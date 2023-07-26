6ea9b1db96f9e0dd6f6a02e89b616c26437017cc
TomasAlric <tomas.alricc@gmail.com>
2023-07-26 02:51:57 -0300

    test: testes unitários

--- test_project.py
+++ test_project.py
@@ -1,53 +1,60 @@
-# test_project
-from project import add_task, view_tasks, complete_task, tasks
+from unittest.mock import patch
+
+import pytest
+
+from project import tasks, add_task, view_tasks, complete_task
+
 
 def test_add_task():
-
-    add_task()
+    tasks.clear()  # Limpar a lista de tarefas no início do teste
+    with patch('builtins.input', side_effect=["Task 1", "Description for Task 1", "2023-07-15", ""]):
+        add_task()
     assert len(tasks) == 1
     assert tasks[0]["name"] == "Task 1"
     assert tasks[0]["description"] == "Description for Task 1"
     assert tasks[0]["due_date"] == "2023-07-15"
     assert tasks[0]["completed"] is False
 
-def test_view_tasks_empty():
 
-    view_tasks()
-    assert "No tasks found." in capsys.readouterr().out
+def test_view_tasks_empty(capsys):
+    tasks.clear()  # Limpar a lista de tarefas no início do teste
+    with patch('builtins.input', return_value=''):
+        view_tasks()
+    captured = capsys.readouterr()
+    assert "No tasks found." in captured.out
 
-def test_view_tasks():
 
-    tasks.clear()
+def test_view_tasks(capsys):
+    tasks.clear()  # Limpar a lista de tarefas no início do teste
     tasks.append({"name": "Task 1", "description": "Description for Task 1", "due_date": "2023-07-15", "completed": False})
     tasks.append({"name": "Task 2", "description": "Description for Task 2", "due_date": "2023-07-16", "completed": True})
+
+    with patch('builtins.input', return_value=''):
+        view_tasks()
 
     view_tasks_output = capsys.readouterr().out
-    assert "Task List:" in view_tasks_output
-    assert "1. Task 1 - Description for Task 1 (Due Date: 2023-07-15) [Not Completed]" in view_tasks_output
-    assert "2. Task 2 - Description for Task 2 (Due Date: 2023-07-16) [Completed]" in view_tasks_output
+    assert "\033[92m\nTask List:\033[0m" in view_tasks_output
+    assert f"1. Task 1 - Description for Task 1 (Due Date: 2023-07-15) [\033[91mNot Completed\033[0m]" in view_tasks_output
+    assert f"2. Task 2 - Description for Task 2 (Due Date: 2023-07-16) [\033[92mCompleted\033[0m]" in view_tasks_output
+
 
 def test_complete_task():
-
-    tasks.clear()
+    tasks.clear()  # Limpar a lista de tarefas no início do teste
     tasks.append({"name": "Task 1", "description": "Description for Task 1", "due_date": "2023-07-15", "completed": False})
-    tasks.append({"name": "Task 2", "description": "Description for Task 2", "due_date": "2023-07-16", "completed": True})
+    tasks.append({"name": "Task 2", "description": "Description for Task 2", "due_date": "2023-07-16", "completed": False})
 
-    complete_task()
-    assert tasks[1]["completed"] is False
+    with patch('builtins.input', return_value="2"):
+        complete_task()
 
-def test_complete_task_invalid_input():
+    assert tasks[1]["completed"] is True
 
-    tasks.clear()
+
+def test_complete_task_invalid_input(capsys):
+    tasks.clear()  # Limpar a lista de tarefas no início do teste
     tasks.append({"name": "Task 1", "description": "Description for Task 1", "due_date": "2023-07-15", "completed": False})
 
-
-    with pytest.raises(ValueError):
-        input_values = ["0"]
-        input_mock = lambda _: input_values.pop(0)
-        builtin_input_mock = "__builtin__.input" if sys.version_info.major < 3 else "builtins.input"
-
-        with patch(builtin_input_mock, input_mock):
+    with patch('builtins.input', side_effect=['0', '']):
+        with pytest.raises(ValueError):
             complete_task()
 
-    
     assert tasks[0]["completed"] is False
